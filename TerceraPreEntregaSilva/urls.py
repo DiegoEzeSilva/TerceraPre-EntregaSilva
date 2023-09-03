232df829385453fc349bb75a7cb03daca064ba0c
Diego Silva <silvadiegoe@gmail.com>
2023-09-03 17:24:19 -0300

    cracion de formularios

--- TerceraPreEntregaSilva/urls.py
+++ TerceraPreEntregaSilva/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from AppTerceraEntrega.views import empleado, lista_empleados
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agrega-empleado/<nombre>/<apellido>/<email>/<celular>', empleado),
-    path('lista_empleados/', lista_empleados)
+    path('AppTerceraEntrega/', include('AppTerceraEntrega.urls'))
 ]
